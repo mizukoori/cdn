1f7d248055cd2fcc4a98c8b954c55d76793f361b
mizukori <me@yuuu.org>
2026-05-18 18:39:22 +0800

    更新脚本，更新字体

--- auto.sh
+++ auto.sh
@@ -1,12 +1,12 @@
 #防止各种github错误
 git config --global core.autocrlf false
 git config --global http.sslVerify false
+# 更新package版本号（不自动commit）
+npm version patch --no-git-tag-version
 # 将更改提交
 git add .
 printf "您的commit的说明："
 read commit
 git commit -m "${commit}"
-# 更新package版本号
-npm version patch
 # 推送至github触发action
 git push
